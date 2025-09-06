3f3fc9e8d6ab0423c9626c18442ee2c80bbfa6f9
Abdur Rehman Imran <arehmanimran4@gmail.com>
2025-09-06 11:50:35 +0500

    Removed some unneeded things

--- string.c
+++ string.c
@@ -7,8 +7,7 @@ typedef char * string_t;
 int countString(char *string)
 {
   int num=0;
-  char c;
-  while (c = string[num++] != 0);
+  while (string[num++] != 0);
   return num-1;
 }
 
@@ -24,19 +23,14 @@ string_t getLine()
   for (int i=0; i<len; i++)
   {
     temp_str[i] = buffer[i];
-    printf("%d : %c\n",i, temp_str[i]);
   }
   temp_str[len] = 0;
-  string = malloc(sizeof(temp_str)*4);
+  string = malloc(sizeof(temp_str));
   strcpy(string, temp_str);
   return string;
 }
 
 int main(){
-  string_t sentence = "Hello World!"; // Direct Initialization
-  string_t empty_init;
-  empty_init = "Abdur Rehman Imran";
-
   string_t input;
 
   input = getLine();
