b3e50066a527638632440d03361037be8d4c8d6e
Abdur Rehman Imran <arehmanimran4@gmail.com>
2025-09-07 15:37:41 +0500

    Added concat function

--- string.c
+++ string.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef char * string_t;
+typedef char *string_t;
 
-int countString(char *string)
-{
-  int num=0;
-  while (string[num++] != 0);
-  return num-1;
+int countString(string_t string) {
+  int num = 0;
+  while (string[num++] != 0)
+    ;
+  return num - 1;
 }
 
-
-string_t getLine()
-{
+string_t getLine() {
   string_t string;
   char buffer[256];
   fgets(buffer, 256, stdin);
   buffer[strcspn(buffer, "\n")] = 0;
   int len = countString(buffer);
-  char temp_str[len+1];
-  for (int i=0; i<len; i++)
-  {
+  char temp_str[len + 1];
+  for (int i = 0; i < len; i++) {
     temp_str[i] = buffer[i];
   }
   temp_str[len] = 0;
@@ -30,10 +27,20 @@ string_t getLine()
   return string;
 }
 
-int main(){
-  string_t input;
+string_t concat(string_t str1, string_t str2) {
+  string_t combined_str;
+  combined_str = (string_t)malloc(sizeof(str1));
+  strcpy(combined_str, str1);
+  strncat(combined_str, str2, strlen(str2));
+  return combined_str;
+}
 
-  input = getLine();
-  printf("%s\n", input);
+int main() {
+  string_t greeting = "Hello ";
+  string_t name;
+
+  name = getLine();
+  printf("%s\n", concat(greeting, name));
+  free(name);
   return 0;
 }
